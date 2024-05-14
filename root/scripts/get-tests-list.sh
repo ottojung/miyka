74648ca465aa03489c19e9b3e04fbb206a42ef2c
Otto Jung <otto.jung@vauplace.com>
2024-05-13 22:02:24 -0700

    add get-tests-list script

---
+++ root/scripts/get-tests-list.sh
@@ -0,0 +1,39 @@
+#! /bin/sh
+
+CI_TESTS="
+"
+
+GUIX_TESTS="
+tests/test-simple-run.sh
+"
+
+if command -v guix 1>/dev/null 2>/dev/null
+then
+	HAVE_GUIX=1
+fi
+
+for FILE in tests/*
+do
+	case "$FILE" in
+		tests/test-*) ;;
+		*) continue ;;
+	esac
+
+	if echo "$GUIX_TESTS" | grep -q -F -e "$FILE"
+	then
+		if ! test "$HAVE_GUIX" = 1
+		then
+			continue
+		fi
+	fi
+
+	if echo "$CI_TESTS" | grep -q -F -e "$FILE"
+	then
+		if ! test "$CI" = 1
+		then
+			continue
+		fi
+	fi
+
+	echo "$FILE"
+done
