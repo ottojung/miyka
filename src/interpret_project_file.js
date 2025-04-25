777b75fd51ff9c8ab3e45c78fa24cb334aee1a89
Otto Jung <otto.jung@vauplace.com>
2025-04-25 16:54:48 -0700

    add a TODO comment

--- src/interpret_project_file.js
+++ src/interpret_project_file.js
@@ -4,5 +4,8 @@
  * @returns {void}
  */
 export function interpretProjectFile(projectFilePath) {
+    // TODO: implement this.
+    // the way it should work is it should `npm install` whatever is in `$(dirpath $projectFilePath)/package.json`.
+    // then it should run that package as a command line script.
     throw new Error('interpretProjectFile not implemented');
 }
