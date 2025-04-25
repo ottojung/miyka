08f05892c3eb39f2991e0146f661149450d7e045
Otto Jung <otto.jung@vauplace.com>
2025-04-25 15:06:09 -0700

    Revert "integrate cac"
    This reverts commit bf4ca2ddd2de916bf1d0c29199fb0452844c2275.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 
-import { cac } from 'cac';
-
 /**
  * Adds two numbers.
  * @param {number} a
@@ -9,17 +7,4 @@ import { cac } from 'cac';
  */
 export function add(a, b) {
     return a + b;
-}
-
-if (typeof require !== 'undefined' && require.main === module) {
-    const cli = cac('miyka');
-
-    cli
-        .command('run <project_name>', 'Run a project')
-        .action((projectName) => {
-            console.log(`Running project ${projectName}`);
-        });
-
-    cli.help();
-    cli.parse();
 }
